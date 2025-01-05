6160bd8d405e7953c7cf346ad1d56c79c5524903
hamzatalha77 <hamza_ta7@hotmail.com>
2025-01-05 19:18:27 +0100

    commit

---
+++ src/controllers/courseController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+
+export const ListCourses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { category } = req.query
+  try {
+  } catch (error) {}
+}
