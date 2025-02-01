a7be291b8b442cdd1e843b5fa6eff4c9eca34646
hamzatalha77 <hamza_ta7@hotmail.com>
2025-02-01 23:30:02 +0100

    commit

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -11,7 +11,7 @@ export const listCourses = async (
       category && category !== 'all'
         ? await Course.scan('category').eq(category).exec()
         : await Course.scan().exec()
-    res.json({ message: 'Courses retrieved successfully ', data: courses })
+    res.json({ message: 'Courses retrieved successfully!', data: courses })
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving courses', error })
   }
